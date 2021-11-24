24cb61a6a31d1b498f6eb74e8c6828a8ff20915c
SamanjaCartagena <cart.samanja09@gmail.com>
2021-11-23 19:52:39 -0500

    boiler plate code

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+//@refresh reset
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, TextInput, View, Button, LogBox } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import *  as firebase from 'firebase';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  
+};
+
+// Initialize Firebase
+if(firebase.apps.length == 0){
+  firebase.initializeApp(firebaseConfig);
+
+}
+LogBox.ignoreWarnings(['Setting a timer for a long period of time'])
 export default function App() {
+  const [user, setUser] = useState(null)
+  const [name,setName]  = useState('');
+  useEffect(()  =>{
+     readUser()
+  },[])
+
+ async function readUser(){
+ const user = await AsyncStorage.getItem('user')
+ if(user){
+   setUser(JSON.parse(user))
+ }
+ }
+ if(!user){
+   return (<View style={styles.container}>
+      <TextInput style={styles.input}  placeholder="Enter your name" value={}/>
+   </View>)
+ }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We have a user!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +49,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:30,
+
   },
+  input:{
+    height:50,
+    width:'100%',
+    borderWidth:1,
+    padding:15,
+    borderColor:'gray'
+
+  }
 });
